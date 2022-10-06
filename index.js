13ab7e4103b724c3422770c8e98fb5cc12e35f48
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-10-06 14:59:53 +0700

    docker compose

--- index.js
+++ index.js
@@ -3,8 +3,32 @@ const mysql = require("mysql2")
 
 const app = express()
 
+const mysqlConfig = {
+  host: "mysql_server",
+  user: "root",
+  password: "password",
+  database: "express_mysql",
+  port: 3306
+}
+
+let db
+
 app.get("/", (req, res) => {
   res.send("<h1>Dockerized API</h1>")
+})
+
+app.post("/connect", (req, res) => {
+  db = mysql.createConnection(mysqlConfig)
+
+  db.connect((err) => {
+    if (err) {
+      console.log(err)
+      return res.status(500).send("MySQL error")
+    } else {
+      console.log("MySQL Connection successful...")
+      return res.status(200).send("MySQL connected")
+    }
+  })
 })
 
 app.listen(2001, () => {
